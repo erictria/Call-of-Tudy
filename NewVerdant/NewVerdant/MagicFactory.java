9f8d0425fd1d9fd7089b8896ba6fcfae41f0365f
xvhcm <harondinvincible@gmail.com>
2016-04-23 23:51:21 +0800

    K

---
+++ NewVerdant/NewVerdant/MagicFactory.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class MagicFactory here.
+ * 
+ * @author Eric Tria 
+ * @version 1.0 Apr 22, 2016
+ */
+public class MagicFactory extends ProjectileFactory
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    public MagicFactory(Map m){
+        super();
+        projectileVel = 10;
+        fireRate = 700;
+        currentMap = m;
+    }
+
+    protected Projectile createProjectile( float x, float y){
+        Projectile sample = new Magic( x, y, 40, 40 );
+        return sample;
+    }
+    
+    public int setMap(Map m)
+    {
+        currentMap = m;
+        return 0;
+    }
+}
